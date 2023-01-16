ed4435fc1252926cdc924278ce9f7d5845f59dc8
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2023-01-16 02:00:41 +0100

    test keys are contigous in tar files

--- tests/test_preffs.py
+++ tests/test_preffs.py
@@ -2,15 +2,16 @@
 reference filesystems based on tar files
 """
 import glob
-import itertools
+import os
 import shutil
-import tarfile
 
+import pandas as pd
 import pytest
 import xarray as xr
 from xarray.testing import assert_equal
 
 import tar_referencer.referencer as r
+import tar_referencer.tar as t
 
 
 @pytest.fixture(scope="session")
@@ -21,40 +22,37 @@ def tmp_test_folder(tmpdir_factory):
 
 
 def create_test_tar(tmp_test_folder):
-    print("helllo")
     xr.tutorial.load_dataset("air_temperature").chunk(
         {"time": 100, "lat": 5, "lon": 5}
     ).to_zarr(str(tmp_test_folder) + "/example.zarr")
-    with tarfile.open(f"{tmp_test_folder}/example.zarr.tar", "w") as tar:
-        zarr_base = f"{tmp_test_folder}/example.zarr/"
-        zarr_files = itertools.chain(
-            glob.iglob(zarr_base + "**", recursive=True),
-            glob.iglob(zarr_base + ".**", recursive=True),
-        )
-        for name in zarr_files:
-            arcname = name.replace(str(tmp_test_folder) + "/example.zarr/", "")
-            if arcname == "":
-                print(name)
-                continue
-            tar.add(name, recursive=False, arcname=arcname)
+    os.chdir(os.path.join(tmp_test_folder, "example.zarr"))
+    t.pack_to_tar("", os.path.join(tmp_test_folder, "example.zarr.{:03d}.tar"), 1000000)
 
 
 def test_preffs_creation(
-    tmp_test_folder, tar_fn="example.zarr.tar", output_folder=tmp_test_folder
+    tmp_test_folder, tar_fn="example.zarr.*.tar", output_folder=tmp_test_folder
 ):
     create_test_tar(tmp_test_folder)
     parquet_filename = f"{tmp_test_folder}/example.zarr.tar.preffs"
-    tar_fn = f"{tmp_test_folder}/{tar_fn}"
-    r.create_preffs(tar_fn, parquet_filename)
+    tar_fn = f"{tar_fn}"
+    os.chdir(tmp_test_folder)
+    r.create_preffs(sorted(glob.glob(tar_fn)), parquet_filename)
 
 
 def test_valid_preffs(
     tmp_test_folder, preffs_fn="example.zarr.tar.preffs", original_fn="example.zarr"
 ):
     storage_options = {"preffs": {"prefix": str(tmp_test_folder) + "/"}}
-    print(storage_options)
     ds_preffs = xr.open_zarr(
         f"preffs::{tmp_test_folder}/{preffs_fn}", storage_options=storage_options
     )
     ds = xr.open_zarr(f"{tmp_test_folder}/{original_fn}")
     assert_equal(ds_preffs, ds)
+
+
+def test_sorted_preffs(tmp_test_folder, preffs_fn="example.zarr.tar.preffs"):
+    df = pd.read_parquet(os.path.join(tmp_test_folder, preffs_fn))
+    assert (
+        len(df.path.unique()) > 1
+    ), "this test cannot be done if only one tar archive is referenced"
+    assert df.path.is_monotonic_increasing, "preffs paths are not sorted by filename"
